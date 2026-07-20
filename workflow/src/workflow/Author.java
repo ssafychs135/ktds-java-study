8264e209f7af179dfe500c4dcd4529e8b5c2bf4f
최형선 <sunnychoi135@naver.com>
2026-07-21 08:58:09 +0900

    260720 homework

---
+++ workflow/src/workflow/Author.java
@@ -0,0 +1,23 @@
+/**
+ * 작가(Author) 클래스
+ * - "사랑스러운 강아지" 페이지의 작가 정보를 필수 항목만 남겨 표현한다.
+ * - 그림(일러스트) 정보는 설계 대상에서 제외한다.
+ * - getter/setter 없이 필드를 public 으로 직접 접근한다.
+ */
+public class Author {
+
+    /** 작가 이름 (예: 몰티즈) */
+    public String name;
+
+    public Author() {
+    }
+
+    public Author(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{name='" + name + "'}";
+    }
+}
---
+++ workflow/src/workflow/Book.java
@@ -0,0 +1,36 @@
+/**
+ * 도서(Book) 클래스
+ * - "사랑스러운 강아지" 페이지의 도서 정보를 필수 항목만 남겨 표현한다.
+ * - 그림(일러스트) 정보는 설계 대상에서 제외한다.
+ * - getter/setter 없이 필드를 public 으로 직접 접근한다.
+ */
+public class Book {
+
+    /** 도서명 */
+    public String title;
+
+    /** 저자 */
+    public String author;
+
+    /** 출판사 */
+    public String publisher;
+
+    /** 가격 */
+    public int price;
+
+    public Book() {
+    }
+
+    public Book(String title, String author, String publisher, int price) {
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{title='" + title + "', author='" + author
+                + "', publisher='" + publisher + "', price=" + price + "}";
+    }
+}
