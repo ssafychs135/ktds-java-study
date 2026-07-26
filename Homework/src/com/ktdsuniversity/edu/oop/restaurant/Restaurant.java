04e92359342f133116757cca68dac3f10703a285
최형선 <sunnychoi135@naver.com>
2026-07-27 08:59:25 +0900

    260727 homework
    레스토랑 메뉴를 이름으로 조회하는 로직 제거.
    findFood/findAlcohol과 null 체크를 없애고 serveFood/serveAlcohol이
    Food/Alcohol 객체를 직접 받도록 변경.

--- Homework/src/com/ktdsuniversity/edu/oop/restaurant/Restaurant.java
+++ Homework/src/com/ktdsuniversity/edu/oop/restaurant/Restaurant.java
@@ -42,17 +42,10 @@ public class Restaurant {
 	/**
 	 * 손님에게 음식을 한 접시 제공한다.
 	 * @param customer 음식을 먹을 손님
-	 * @param foodName 제공할 음식 이름
+	 * @param food 제공할 음식
 	 * @return 제공 성공 여부
 	 */
-	public boolean serveFood(Customer customer, String foodName) {
-
-		Food food = findFood(foodName);
-
-		if (food == null) {
-			System.out.println(foodName + "은(는) 저희 레스토랑에서 제공하지 않습니다.");
-			return false;
-		}
+	public boolean serveFood(Customer customer, Food food) {
 
 		return customer.eat(food);
 	}
@@ -61,17 +54,10 @@ public class Restaurant {
 	 * 손님에게 주류를 한 병 제공한다.
 	 * 19세 미만의 미성년자에게는 제공하지 않으나, 부모님과 동반한 경우에는 제공한다.
 	 * @param customer 주류를 마실 손님
-	 * @param alcoholName 제공할 주류 이름
+	 * @param alcohol 제공할 주류
 	 * @return 제공 성공 여부
 	 */
-	public boolean serveAlcohol(Customer customer, String alcoholName) {
-
-		Alcohol alcohol = findAlcohol(alcoholName);
-
-		if (alcohol == null) {
-			System.out.println(alcoholName + "은(는) 저희 레스토랑에서 제공하지 않습니다.");
-			return false;
-		}
+	public boolean serveAlcohol(Customer customer, Alcohol alcohol) {
 
 		if (customer.getAge() < ADULT_AGE && !customer.isWithParent()) {
 			System.out.println(customer.getName() + "님은 미성년자이므로 주류를 제공해 드릴 수 없습니다."
@@ -80,38 +66,6 @@ public class Restaurant {
 		}
 
 		return customer.drink(alcohol);
-	}
-
-	/**
-	 * 이름으로 음식을 찾는다.
-	 * @param foodName 찾을 음식 이름
-	 * @return 찾은 음식. 없으면 null
-	 */
-	private Food findFood(String foodName) {
-
-		for (int i = 0; i < this.foods.length; i++) {
-			if (this.foods[i].getName().equals(foodName)) {
-				return this.foods[i];
-			}
-		}
-
-		return null;
-	}
-
-	/**
-	 * 이름으로 주류를 찾는다.
-	 * @param alcoholName 찾을 주류 이름
-	 * @return 찾은 주류. 없으면 null
-	 */
-	private Alcohol findAlcohol(String alcoholName) {
-
-		for (int i = 0; i < this.alcohols.length; i++) {
-			if (this.alcohols[i].getName().equals(alcoholName)) {
-				return this.alcohols[i];
-			}
-		}
-
-		return null;
 	}
 
 	public void printMenu() {
@@ -139,6 +93,14 @@ public class Restaurant {
 
 	public String getName() {
 		return name;
+	}
+
+	public Food[] getFoods() {
+		return foods;
+	}
+
+	public Alcohol[] getAlcohols() {
+		return alcohols;
 	}
 
 }
--- Homework/src/com/ktdsuniversity/edu/oop/restaurant/RestaurantMain.java
+++ Homework/src/com/ktdsuniversity/edu/oop/restaurant/RestaurantMain.java
@@ -14,49 +14,45 @@ public class RestaurantMain {
 		Customer 박동반 = new Customer("박동반", 17, true);
 
 		System.out.println("\n===== 1. 성인 손님의 식사 =====");
-		restaurant.serveFood(김성인, "육고기");
-		restaurant.serveFood(김성인, "생선");
-		restaurant.serveAlcohol(김성인, "소주");
-		restaurant.serveAlcohol(김성인, "맥주");
+		restaurant.serveFood(김성인, restaurant.getFoods()[0]);        // 육고기
+		restaurant.serveFood(김성인, restaurant.getFoods()[3]);        // 생선
+		restaurant.serveAlcohol(김성인, restaurant.getAlcohols()[0]);  // 소주
+		restaurant.serveAlcohol(김성인, restaurant.getAlcohols()[1]);  // 맥주
 		김성인.printStatus();
 
 		System.out.println("\n===== 2. 미성년자는 주류를 마실 수 없다 =====");
-		restaurant.serveFood(이청소, "육고기");
-		restaurant.serveAlcohol(이청소, "소주");
+		restaurant.serveFood(이청소, restaurant.getFoods()[0]);        // 육고기
+		restaurant.serveAlcohol(이청소, restaurant.getAlcohols()[0]);  // 소주
 		이청소.printStatus();
 
 		System.out.println("\n===== 3. 부모님과 동반한 미성년자는 주류를 마실 수 있다 =====");
-		restaurant.serveAlcohol(박동반, "맥주");
+		restaurant.serveAlcohol(박동반, restaurant.getAlcohols()[1]);  // 맥주
 		박동반.printStatus();
 
 		System.out.println("\n===== 4. 취기가 60%를 초과하면 더 이상 마실 수 없다 =====");
-		restaurant.serveAlcohol(김성인, "위스키");
-		restaurant.serveAlcohol(김성인, "꼬냑");
-		restaurant.serveAlcohol(김성인, "소주");
+		restaurant.serveAlcohol(김성인, restaurant.getAlcohols()[2]);  // 위스키
+		restaurant.serveAlcohol(김성인, restaurant.getAlcohols()[3]);  // 꼬냑
+		restaurant.serveAlcohol(김성인, restaurant.getAlcohols()[0]);  // 소주
 		김성인.printStatus();
 
 		System.out.println("\n===== 5. 채소와 과일은 취기를 떨어트린다 =====");
-		restaurant.serveFood(김성인, "채소");
-		restaurant.serveFood(김성인, "과일");
-		restaurant.serveFood(김성인, "채소");
+		restaurant.serveFood(김성인, restaurant.getFoods()[1]);        // 채소
+		restaurant.serveFood(김성인, restaurant.getFoods()[2]);        // 과일
+		restaurant.serveFood(김성인, restaurant.getFoods()[1]);        // 채소
 		김성인.printStatus();
 
 		System.out.println("\n===== 6. 포만감이 100%를 초과하면 더 이상 먹을 수 없다 =====");
-		restaurant.serveFood(김성인, "육고기");
-		restaurant.serveFood(김성인, "육고기");
-		restaurant.serveFood(김성인, "육고기");
-		restaurant.serveFood(김성인, "육고기");
-		restaurant.serveFood(김성인, "생선");
+		restaurant.serveFood(김성인, restaurant.getFoods()[0]);        // 육고기
+		restaurant.serveFood(김성인, restaurant.getFoods()[0]);        // 육고기
+		restaurant.serveFood(김성인, restaurant.getFoods()[0]);        // 육고기
+		restaurant.serveFood(김성인, restaurant.getFoods()[0]);        // 육고기
+		restaurant.serveFood(김성인, restaurant.getFoods()[3]);        // 생선
 		김성인.printStatus();
 
 		System.out.println("\n===== 7. 맥주를 제외한 주류는 포만감을 떨어트린다 =====");
-		restaurant.serveAlcohol(박동반, "소주");
-		restaurant.serveAlcohol(박동반, "맥주");
+		restaurant.serveAlcohol(박동반, restaurant.getAlcohols()[0]);  // 소주
+		restaurant.serveAlcohol(박동반, restaurant.getAlcohols()[1]);  // 맥주
 		박동반.printStatus();
-
-		System.out.println("\n===== 8. 메뉴에 없는 음식 =====");
-		restaurant.serveFood(김성인, "떡볶이");
-		restaurant.serveAlcohol(김성인, "막걸리");
 
 		System.out.println("\n===== 최종 상태 =====");
 		김성인.printStatus();
