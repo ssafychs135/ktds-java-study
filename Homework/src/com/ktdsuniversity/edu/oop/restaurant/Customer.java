6640101cdd3be6c7533bf727cfbdbccd819e69df
최형선 <sunnychoi135@naver.com>
2026-07-27 09:25:51 +0900

    260727 homework
    포만감/취기 상한 정책을 Customer에서 Restaurant로 이동.
    MAX_FULLNESS, MAX_DRUNKENNESS 상수와 제공 거절 판단,
    손님 안내 출력을 모두 Restaurant가 담당하도록 변경.
    Customer.eat/drink는 상태 변경만 하는 void 메소드가 됨.

--- Homework/src/com/ktdsuniversity/edu/oop/restaurant/Customer.java
+++ Homework/src/com/ktdsuniversity/edu/oop/restaurant/Customer.java
@@ -3,14 +3,9 @@ package com.ktdsuniversity.edu.oop.restaurant;
 /**
  * 레스토랑을 방문한 손님.
  * 자신의 포만감과 취기를 스스로 관리한다.
+ * 먹을 수 있는지, 마실 수 있는지는 레스토랑이 판단한다.
  */
 public class Customer {
-
-	/** 포만감이 이 수치를 초과하면 더 이상 먹을 수 없다. */
-	private static final int MAX_FULLNESS = 100;
-
-	/** 취기가 이 수치를 초과하면 더 이상 마실 수 없다. */
-	private static final int MAX_DRUNKENNESS = 60;
 
 	/** 손님 이름 */
 	private String name;
@@ -36,15 +31,8 @@ public class Customer {
 	/**
 	 * 음식을 한 접시 먹는다.
 	 * @param food 먹을 음식
-	 * @return 먹었으면 true, 배가 불러 먹지 못했으면 false
 	 */
-	public boolean eat(Food food) {
-
-		if (this.fullness > MAX_FULLNESS) {
-			System.out.println(this.name + "님은 배가 너무 불러 " + food.getName() + "을(를) 드실 수 없습니다."
-					+ " (포만감: " + this.fullness + "%)");
-			return false;
-		}
+	public void eat(Food food) {
 
 		this.fullness += food.getFullnessUp();
 
@@ -54,25 +42,13 @@ public class Customer {
 		if (this.drunkenness < 0) {
 			this.drunkenness = 0;
 		}
-
-		System.out.println(this.name + "님이 " + food.getName() + "을(를) 한 접시 드셨습니다."
-				+ " (포만감: " + this.fullness + "% / 취기: " + this.drunkenness + "%)");
-
-		return true;
 	}
 
 	/**
 	 * 주류를 한 병 마신다.
 	 * @param alcohol 마실 주류
-	 * @return 마셨으면 true, 취기가 높아 마시지 못했으면 false
 	 */
-	public boolean drink(Alcohol alcohol) {
-
-		if (this.drunkenness > MAX_DRUNKENNESS) {
-			System.out.println(this.name + "님은 취기가 올라 " + alcohol.getName() + "을(를) 드실 수 없습니다."
-					+ " (취기: " + this.drunkenness + "%)");
-			return false;
-		}
+	public void drink(Alcohol alcohol) {
 
 		this.drunkenness += alcohol.getDrunkennessUp();
 
@@ -83,11 +59,6 @@ public class Customer {
 		if (this.fullness < 0) {
 			this.fullness = 0;
 		}
-
-		System.out.println(this.name + "님이 " + alcohol.getName() + "을(를) 한 병 드셨습니다."
-				+ " (포만감: " + this.fullness + "% / 취기: " + this.drunkenness + "%)");
-
-		return true;
 	}
 
 	public void printStatus() {
--- Homework/src/com/ktdsuniversity/edu/oop/restaurant/Restaurant.java
+++ Homework/src/com/ktdsuniversity/edu/oop/restaurant/Restaurant.java
@@ -2,12 +2,18 @@ package com.ktdsuniversity.edu.oop.restaurant;
 
 /**
  * 웰빙 무한 레스토랑.
- * 음식과 주류를 손님에게 제공한다.
+ * 음식과 주류를 손님에게 제공하며, 제공 가능 여부를 정책에 따라 판단한다.
  */
 public class Restaurant {
 
 	/** 이 나이 미만은 미성년자로, 주류를 제공하지 않는다. */
 	private static final int ADULT_AGE = 19;
+
+	/** 손님의 포만감이 이 수치를 초과하면 음식을 제공하지 않는다. */
+	private static final int MAX_FULLNESS = 100;
+
+	/** 손님의 취기가 이 수치를 초과하면 주류를 제공하지 않는다. */
+	private static final int MAX_DRUNKENNESS = 60;
 
 	/** 레스토랑 이름 */
 	private String name;
@@ -41,18 +47,31 @@ public class Restaurant {
 
 	/**
 	 * 손님에게 음식을 한 접시 제공한다.
+	 * 포만감이 100%를 초과한 손님에게는 제공하지 않는다.
 	 * @param customer 음식을 먹을 손님
 	 * @param food 제공할 음식
 	 * @return 제공 성공 여부
 	 */
 	public boolean serveFood(Customer customer, Food food) {
 
-		return customer.eat(food);
+		if (customer.getFullness() > MAX_FULLNESS) {
+			System.out.println(customer.getName() + "님은 배가 너무 불러 " + food.getName() + "을(를) 드실 수 없습니다."
+					+ " (포만감: " + customer.getFullness() + "%)");
+			return false;
+		}
+
+		customer.eat(food);
+
+		System.out.println(customer.getName() + "님이 " + food.getName() + "을(를) 한 접시 드셨습니다."
+				+ " (포만감: " + customer.getFullness() + "% / 취기: " + customer.getDrunkenness() + "%)");
+
+		return true;
 	}
 
 	/**
 	 * 손님에게 주류를 한 병 제공한다.
 	 * 19세 미만의 미성년자에게는 제공하지 않으나, 부모님과 동반한 경우에는 제공한다.
+	 * 취기가 60%를 초과한 손님에게도 제공하지 않는다.
 	 * @param customer 주류를 마실 손님
 	 * @param alcohol 제공할 주류
 	 * @return 제공 성공 여부
@@ -65,7 +84,18 @@ public class Restaurant {
 			return false;
 		}
 
-		return customer.drink(alcohol);
+		if (customer.getDrunkenness() > MAX_DRUNKENNESS) {
+			System.out.println(customer.getName() + "님은 취기가 올라 " + alcohol.getName() + "을(를) 드실 수 없습니다."
+					+ " (취기: " + customer.getDrunkenness() + "%)");
+			return false;
+		}
+
+		customer.drink(alcohol);
+
+		System.out.println(customer.getName() + "님이 " + alcohol.getName() + "을(를) 한 병 드셨습니다."
+				+ " (포만감: " + customer.getFullness() + "% / 취기: " + customer.getDrunkenness() + "%)");
+
+		return true;
 	}
 
 	public void printMenu() {
